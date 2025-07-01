80f1849afe53ad75ee78f037ec463d217997a4df
kikisan <kiketdule@gmail.com>
2025-07-01 06:39:00 +0200

    affichage nom liste dans desktop content

--- src/components/desktop/DesktopContent.tsx
+++ src/components/desktop/DesktopContent.tsx
@@ -1,29 +1,60 @@
-import { useAuthStore } from "@/Context/useAuthStore";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useListsStore } from "@/Context/useListsStore";
 import { useUserContextStore } from "@/Context/useUserContextStore";
+import type { AccessList, OwnedList } from "@/types/List";
 import type { User } from "@/types/User";
-
-
+import { useEffect, useState } from "react";
 
 interface DesktopContentProps {
   user: User;
 }
 
 export default function DesktopContent({ user }: DesktopContentProps) {
-
   const selectedListId = useUserContextStore((state) => state.selectedListId);
-  
-  return (
-    <div className="mx-auto w-full max-w-screen-xl flex flex-col">
-      <div>DesktopContent</div>
-      {user && (
-        <div className="p-4">
-          <h2 className="text-2xl font-bold text-amber-100">Bienvenue, {user.name} !</h2>
-          <p>ton id est {user.id}</p>
-          <p>ton email est {user.email}</p>
-          <p>ton token est {useAuthStore.getState().token?.length}</p>
-          <p>Ta liste a l'ID : {selectedListId}</p>
-        </div>
-      )}
-    </div>
+  const { ownedLists, accessLists } = useListsStore();
+  const [currentList, setCurrentList] = useState<OwnedList | AccessList | null>(
+    null
   );
+
+  console.log("accessLists:", accessLists);
+  console.log("ownedLists:", ownedLists);
+
+  useEffect(() => {
+    setCurrentList(
+      ownedLists.find((list) => list.id === selectedListId) ||
+        accessLists.find((list) => list.id === selectedListId) ||
+        null
+    );
+
+    console.log("currentList mis à jour :", currentList);
+  }, [selectedListId, ownedLists, accessLists]);
+
+  if (selectedListId === undefined || selectedListId === null) {
+    return (
+      <div className="p-4 text-amber-100 grow text-center">
+        <h2 className="text-2xl font-bold text-amber-100 my-5">
+          Bienvenue, {user.name} !
+        </h2>
+        {ownedLists.length === 0 && accessLists.length === 0 ? (
+          <p className="">
+            Tu n'as pas encore de listes. Crée-en une pour commencer !
+          </p>
+        ) : (
+          <p className="text-xl mt-3">
+            Sélectionne une liste à partager pour commencer !
+          </p>
+        )}
+      </div>
+    );
+  }
+  if (currentList) {
+    return (
+      <div className="p-4 text-amber-100 grow text-center">
+        <h2 className="text-2xl font-bold text-amber-100 my-5">
+          Bienvenue, {user.name} !
+        </h2>
+        {currentList.name}
+      </div>
+    );
+  }
 }
